9e576b6354b3f237a8708b697fe78f3bc6612060
varalakshmi7654 <varalakshmiveguru955@gmail.com>
2025-03-30 20:22:40 +0530

    Create Prog3.c

---
+++ Prog3.c
@@ -0,0 +1,11 @@
+include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter a number");
+    scanf("%d",&n);
+    if(n%2==0)
+    printf("the given number is an even number");
+    if(n%2==1)
+    printf("the given number is an odd number");
+}
